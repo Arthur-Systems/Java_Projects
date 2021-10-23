a21af2340ba8932637b0f61d51b7f98a8308510f
Arthur <Arthur.wei50@gmail.com>
2021-10-23 11:31:07 -0700

    Midterm Lol

---
+++ Eclipse-Workspace/Midterm/src/main.java
@@ -0,0 +1,22 @@
+
+public class main {
+
+	public static void main(String[] args) {
+
+		// Convert the following for loop to a while loop:
+
+		// for (int x = 100; x > 0; x--) {
+
+		// System.out.println(x + " seconds to go...");
+
+		// }
+
+		int x = 100;
+		while (x > 0) {
+			System.out.println(x + " seconds to go...");
+			x--;
+		}
+
+	}
+
+}
